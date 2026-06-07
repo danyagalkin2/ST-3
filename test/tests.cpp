4510565327bcc7a40e0a7225f9e51db131c5f629
Danya Galkin <danya.galkin2@gmail.com>
2026-06-07 18:33:22 +0300

    st-3

--- test/tests.cpp
+++ test/tests.cpp
@@ -5,6 +5,7 @@
 
 #include <chrono>
 #include <stdexcept>
+#include <string>
 #include <thread>
 
 #include "TimedDoor.h"
@@ -135,4 +136,163 @@ TEST(MockDoorTest, IsDoorOpenedReturnsFalse) {
   NiceMock<MockDoor> door;
   ON_CALL(door, isDoorOpened()).WillByDefault(Return(false));
   ASSERT_FALSE(door.isDoorOpened());
+}
+
+TEST_F(DoorFixture, GetTimeoutOnFastDoor) {
+  EXPECT_EQ(0, fastDoor->getTimeOut());
+}
+
+TEST_F(DoorFixture, ExceptionMessageContainsExpectedText) {
+  try {
+    fastDoor->unlock();
+    FAIL() << "Expected std::runtime_error";
+  } catch (const std::runtime_error& e) {
+    EXPECT_NE(std::string(e.what()).find("timeout"), std::string::npos);
+  }
+}
+
+TEST_F(DoorFixture, IsDoorOpenedAfterUnlock) {
+  std::thread t([this] { slowDoor->unlock(); });
+  std::this_thread::sleep_for(std::chrono::milliseconds(15));
+  EXPECT_TRUE(slowDoor->isDoorOpened());
+  slowDoor->lock();
+  t.join();
+}
+
+TEST_F(DoorFixture, LockAfterUnlockRestoresClosed) {
+  std::thread t([this] { slowDoor->unlock(); });
+  std::this_thread::sleep_for(std::chrono::milliseconds(15));
+  slowDoor->lock();
+  t.join();
+  EXPECT_FALSE(slowDoor->isDoorOpened());
+}
+
+TEST(TimedDoorTest, ThrowStateDoesNotThrowOnFreshDoor) {
+  TimedDoor door(5);
+  EXPECT_NO_THROW(door.throwState());
+}
+
+TEST_F(DoorFixture, MultipleLockUnlockCyclesNoThrow) {
+  slowDoor->lock();
+  EXPECT_FALSE(slowDoor->isDoorOpened());
+  std::thread t([this] { slowDoor->unlock(); });
+  std::this_thread::sleep_for(std::chrono::milliseconds(15));
+  slowDoor->lock();
+  t.join();
+  EXPECT_FALSE(slowDoor->isDoorOpened());
+}
+
+TEST_F(DoorFixture, AdapterTimeoutCalledTwiceOnClosedDoor) {
+  DoorTimerAdapter adapter(*fastDoor);
+  fastDoor->lock();
+  EXPECT_NO_THROW(adapter.Timeout());
+  EXPECT_NO_THROW(adapter.Timeout());
+}
+
+TEST(TimedDoorTest, NewDoorWithLongTimeoutIsClosedInitially) {
+  TimedDoor door(100);
+  EXPECT_EQ(100, door.getTimeOut());
+  EXPECT_FALSE(door.isDoorOpened());
+}
+
+TEST_F(DoorFixture, UnlockThrowsStdRuntimeErrorType) {
+  ASSERT_THROW(fastDoor->unlock(), std::runtime_error);
+}
+
+TEST(TimerTest, TimerRegisterCallsTimeoutWithNonZeroTimeout) {
+  Timer timer;
+  MockTimerClient client;
+  EXPECT_CALL(client, Timeout()).Times(Exactly(1));
+  timer.tregister(1, &client);
+}
+
+TEST(MockDoorTest, IsDoorOpenedReturnsTrue) {
+  NiceMock<MockDoor> door;
+  ON_CALL(door, isDoorOpened()).WillByDefault(Return(true));
+  ASSERT_TRUE(door.isDoorOpened());
+}
+
+TEST(TimerTest, TimerDoesNotCallNullClient) {
+  Timer timer;
+  EXPECT_NO_THROW(timer.tregister(0, nullptr));
+}
+
+TEST_F(DoorFixture, AdapterReferenceTracksLockState) {
+  DoorTimerAdapter adapter(*slowDoor);
+  EXPECT_NO_THROW(adapter.Timeout());
+  std::thread t([this] { slowDoor->unlock(); });
+  std::this_thread::sleep_for(std::chrono::milliseconds(15));
+  EXPECT_THROW(adapter.Timeout(), std::runtime_error);
+  slowDoor->lock();
+  t.join();
+  EXPECT_NO_THROW(adapter.Timeout());
+}
+
+TEST_F(DoorFixture, LockIdempotentOnOpenDoor) {
+  std::thread t([this] { slowDoor->unlock(); });
+  std::this_thread::sleep_for(std::chrono::milliseconds(15));
+  slowDoor->lock();
+  slowDoor->lock();
+  t.join();
+  EXPECT_FALSE(slowDoor->isDoorOpened());
+}
+
+TEST_F(DoorFixture, GetTimeoutUnchangedAfterUnlockLock) {
+  std::thread t([this] { slowDoor->unlock(); });
+  std::this_thread::sleep_for(std::chrono::milliseconds(15));
+  slowDoor->lock();
+  t.join();
+  EXPECT_EQ(60, slowDoor->getTimeOut());
+}
+
+// --- Parameterized tests ---
+
+class TimedDoorParamTest : public ::testing::TestWithParam<int> {};
+
+TEST_P(TimedDoorParamTest, TimeoutStoredForAnyValue) {
+  int timeout = GetParam();
+  TimedDoor door(timeout);
+  EXPECT_EQ(timeout, door.getTimeOut());
+  EXPECT_FALSE(door.isDoorOpened());
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    TimeoutValues,
+    TimedDoorParamTest,
+    ::testing::Values(0, 1, 5, 10, 50, 100, 500, 1000));
+
+// --- MiddleDoorFixture (30ms timeout) ---
+
+class MiddleDoorFixture : public ::testing::Test {
+ protected:
+  TimedDoor* door{};
+  void SetUp() override { door = new TimedDoor(30); }
+  void TearDown() override { delete door; }
+};
+
+TEST_F(MiddleDoorFixture, MiddleTimeoutStoredCorrectly) {
+  EXPECT_EQ(30, door->getTimeOut());
+}
+
+TEST_F(MiddleDoorFixture, MiddleDoorThrowsIfLeftOpen) {
+  std::thread t([this] { door->unlock(); });
+  std::this_thread::sleep_for(std::chrono::milliseconds(10));
+  EXPECT_THROW(door->throwState(), std::runtime_error);
+  door->lock();
+  t.join();
+}
+
+TEST_F(MiddleDoorFixture, MiddleDoorNoThrowIfLockedEarly) {
+  std::thread t([this] { door->unlock(); });
+  std::this_thread::sleep_for(std::chrono::milliseconds(10));
+  door->lock();
+  t.join();
+  EXPECT_FALSE(door->isDoorOpened());
+}
+
+// --- Destructor test ---
+
+TEST(TimedDoorTest, DestructorReleasesAdapter) {
+  { TimedDoor door(5); }
+  SUCCEED();
 }
